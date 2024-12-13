a58a4ddcb6e03d1d909c283fd667b5c7b557b6f5
Anton Log <Antuandelog@outlook.com>
2024-12-13 18:18:46 +0300

    fix: add github cors domain

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const cookieParser = require('cookie-parser');
 const app = express();
 
 let corsOptions = {
-  origin: 'https://cultured-abaft-antimony.glitch.me',
+  origin: [
+    'https://cultured-abaft-antimony.glitch.me',
+    'https://sirgeckt0r.github.io',
+  ],
 };
 
 app.use(cors(corsOptions));
@@ -20,17 +23,13 @@ const Role = db.role;
 
 const PORT = process.env.PORT || 8888;
 app.listen(PORT, () => {
-  if (process.env.NODE_ENV == 'test') {
-    console.log(`Server is running on port ${PORT}.`);
-  }
+  console.log(`Server is running on port ${PORT}.`);
 });
 
-if (process.env.NODE_ENV == 'test') {
-  db.sequelize.sync({ force: true }).then(() => {
-    console.log('Drop and Resync Database with { force: true }');
-    initial();
-  });
-}
+db.sequelize.sync({ force: true }).then(() => {
+  console.log('Drop and Resync Database with { force: true }');
+  initial();
+});
 app.get('/', (req, res) => {
   res.json({ message: 'Test lab 6!' });
 });
